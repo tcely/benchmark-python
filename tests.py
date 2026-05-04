3e34dda2d78d9973403a65b14cef051995675abc
tcely <tcely@users.noreply.github.com>
2026-05-04 06:01:15 -0400

    Fix import statement for MemoryFormatIOBackend

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 import io
-from _io import MemoryFormatIOBackend
+from ._io import MemoryFormatIOBackend
 
 class TestMemoryBackend(unittest.TestCase):
     def setUp(self):
